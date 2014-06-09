530a0f939d0c6c3fec85ab867c0ea8d7bc62852b
Josh Hogan <josh.hogan@me.com>
2014-06-08 22:40:11 -0400

    Another day

--- book.playground/section-1.swift
+++ book.playground/section-1.swift
@@ -587,6 +587,150 @@ anyCommonElements([1, 2, 3], [3])
 commonElements([1,2,3], [3,4,1])
 
 
+// Constants and Variables
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+// Type Annotations
+
+var welcomeMessage: String
+
+welcomeMessage = "Hello"
+
+// Naming Constants and Variables
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+let languageName = "Swift"
+//languageName = "Swift++" - NOPE
+
+println(friendlyWelcome)
+print(friendlyWelcome)
+
+println("The current value of friendlyWelcome is \(friendlyWelcome)")
+
+
+// Integer Bounds
+
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
+
+// Floating Point Type Inference
+// Swift chooses Double (in ambiguous Float situations)
+
+let anotherPi = 3 + 0.14159
+
+
+// Numeric Literals
+
+let decimalInteger = 17
+let binaryInteger = 0b10001
+let octalInteger = 0o21
+let hexadecimalInteger = 0x11
+
+// Integer Conversion
+
+//let cannotBeNegative: UInt8 = -1  - NOPE
+
+//let tooBig: Int8 = Int8.max + 1 - NOPE
+
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+let _16Max = UInt16.max
+let _one: UInt8 = 1
+//let _16MaxAndOne = _16Max + UInt16(_one) // Broken?
+
+// Integer and Floating-Point Conversion
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+
+let integerPi = Int(pi) // floating points are always truncated when initializing an int
+
+// Type Aliases
+
+typealias AudioSample = UInt16
+
+var maxAmplitudeFound = AudioSample.min
+
+// Boolean
+
+/*
+let i = 1
+if i {
+    // err : Type Int does not conform to protocol 'LogicValue'
+}
+*/
+
+let i = 1
+if i == 1 {
+    // all good
+}
+
+// Tuples
+
+let http404Error = (404, "Not Found")
+
+let (statusCode, statusMessage) = http404Error
+println("The status code is \(statusCode)")
+println("The status message is \(statusMessage)")
+
+let (justTheStatusCode, _) = http404Error
+println("The status code is \(justTheStatusCode)")
+
+println("The status code is \(http404Error.0)")
+println("The status message is \(http404Error.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+println("The status code is \(http200Status.statusCode)")
+println("The status message is \(http200Status.description)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
